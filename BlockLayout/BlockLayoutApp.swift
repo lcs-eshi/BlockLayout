09bdefd77094b02aadf1bad147026d7c06205945
Ethan Shi <eshi@lcs.on.ca>
2026-02-10 14:33:45 -0500

    Initial Commit

---
+++ BlockLayout/BlockLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockLayoutApp.swift
+//  BlockLayout
+//
+//  Created by Yicheng Shi on 2026-02-10.
+//
+
+import SwiftUI
+
+@main
+struct BlockLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
