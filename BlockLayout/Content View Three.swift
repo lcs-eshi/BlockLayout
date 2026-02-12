98fb533ac92ff4982029092b85a49a0f47542f47
Ethan Shi <eshi@lcs.on.ca>
2026-02-11 21:58:13 -0500

    Completed third block layout.

---
+++ BlockLayout/Content View Three.swift
@@ -0,0 +1,25 @@
+//
+//  Content View Three.swift
+//  BlockLayout
+//
+//  Created by Yicheng Shi on 2026-02-11.
+//
+
+import SwiftUI
+
+struct Content_View_Three: View {
+    var body: some View {
+        HStack {
+            Rectangle()
+            VStack {
+                Rectangle()
+                Rectangle()
+            }
+                
+        }
+    }
+}
+
+#Preview {
+    Content_View_Three()
+}
